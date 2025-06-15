77721b95b8aac80d95883ac78a2b73fa2e1b0727
Dinesh Babu Vaddineni <89186228+dineshbabuvaddineni@users.noreply.github.com>
2025-06-15 19:05:12 +0530

    Time: 1 ms (99.93%), Space: 46.2 MB (46.64%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,18 +1,20 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashSet<Integer> set=new HashSet<>();
+        // HashSet<Integer> set=new HashSet<>();
+        // for(int num:nums){
+        //     if(set.contains(num)){
+        //         set.remove(num);
+        //     }
+        //     else{
+        //         set.add(num);
+        //     }
+
+        // }
+        // return set.iterator().next();
+        int ans=0;
         for(int num:nums){
-            if(set.contains(num)){
-                set.remove(num);
-            }
-            else{
-                set.add(num);
-            }
-
+            ans=ans^num;
         }
-        return set.iterator().next();
-
-     
-        
+        return ans;     
     }
 }
