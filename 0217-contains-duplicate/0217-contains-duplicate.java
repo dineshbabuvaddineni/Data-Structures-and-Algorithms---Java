fac25d033bea77e3b894af27d00f1169841ca60f
Dinesh Babu Vaddineni <89186228+dineshbabuvaddineni@users.noreply.github.com>
2025-06-15 20:00:42 +0530

    Time: 21 ms (20.48%), Space: 58.2 MB (62.94%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,21 +1,29 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-         Map<Integer,Integer> map=new HashMap<>();
-        for(int a:nums){
-            if(!map.containsKey(a)){
-                map.put(a,1);
-            }else{
-                map.put(a,map.get(a)+1);
-            }
-        }
-        boolean answer=false;
-        for(int key: map.keySet()){
-            if(map.get(key)>1){
-                answer=true;
+        //  Map<Integer,Integer> map=new HashMap<>();
+        // for(int a:nums){
+        //     if(!map.containsKey(a)){
+        //         map.put(a,1);
+        //     }else{
+        //         map.put(a,map.get(a)+1);
+        //     }
+        // }
+        // boolean answer=false;
+        // for(int key: map.keySet()){
+        //     if(map.get(key)>1){
+        //         answer=true;
                 
+        //     }
+        // }
+        // return answer;
+        boolean ans=false;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                ans=true;
             }
         }
-        return answer;
+        return ans;
         
     }
 }
